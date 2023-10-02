84dccae952b9f492faca37884e848a7bc324f561
Tomas Jelinek <jeldicek@seznam.cz>
2023-10-02 23:35:57 +0200

    tests

--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -1,8 +1,11 @@
 import { test, expect } from '@playwright/test';
 import { HomePage } from './pageObjects/homePage.desktop';
-import config from '../playwright.config';
 
 test.describe('Home page basic tests', () => {
+    const HOME_PAGE_TITLE = 'E.ON Drive - Váš partner pro elektromobilitu';
+    const STATION_MAP_TITLE = 'Mapa stanic a dobíjení | EON Drive';
+    const ABOUT_US_TITLE = 'O nás - E.ON Drive';
+
     test.beforeEach(async ({ page }) => {
         await page.goto('/');
     });
@@ -12,14 +15,20 @@ test.describe('Home page basic tests', () => {
     });
 
     test('has specific title', async ({ page }) => {
-        await expect(page).toHaveTitle('E.ON Drive - Váš partner pro elektromobilitu');
+        await expect(page).toHaveTitle(HOME_PAGE_TITLE);
     });
 
     test.describe('Navigation tests', () => {
         test('Go to station map', async ({ page }) => {
             const homePage = new HomePage(page);
             await homePage.header.clickToNavigate(homePage.header.locatorStationMap, 'https://www.eon-drive.cz/mapa/');
-            await expect(page).toHaveTitle('Mapa stanic a dobíjení | EON Drive');
+            await expect(page).toHaveTitle(STATION_MAP_TITLE);
+        });
+
+        test('Go to about us', async ({ page }) => {
+            const homePage = new HomePage(page);
+            await homePage.header.clickToNavigate(homePage.header.locatorStationMap, 'https://www.eon-drive.cz/o-nas/');
+            await expect(page).toHaveTitle(ABOUT_US_TITLE);
         });
     });
 });
